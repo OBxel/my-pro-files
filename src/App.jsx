99b588cec95e723569584783437c67faea0b7648
OBxel <olivier.a.brule@gmail.com>
2024-05-16 03:44:58 +0200

    changement de lien du site

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import Navbar from "./components/Nav/NavBar";
 import Home from "./pages/Home/Home";
 import Projects from "./pages/Projects/Projects";
@@ -12,6 +17,10 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/my-pro-files/home" element={<Home />} />
+          <Route
+            path="/my-pro-files"
+            element={<Navigate to="/my-pro-files/home" />}
+          />
           <Route path="/my-pro-files/projects" element={<Projects />} />
           <Route path="/my-pro-files/contact" element={<Contact />} />
         </Routes>
